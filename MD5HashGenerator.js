6b90b4a3fcf954f9e2a205fe39ba24a57c85497b
Subham Mitra <subham.mitra21@gmail.com>
2021-02-10 18:40:09 +0530

    layout changed

--- MD5HashGenerator.js
+++ MD5HashGenerator.js
@@ -1,6 +1,12 @@
 const input=document.querySelector('#input');
 const output=document.querySelector('#output');
 const button=document.querySelector('#generate-button');
+const resetButton=document.querySelector('#reset-button');
+
+resetButton.addEventListener('click',()=>{
+  input.value="";
+  output.value="";
+  });
 button.addEventListener('click',()=>{
   output.value=CryptoJS.MD5(input.value);
   
--- SHA1HashGenerator.js
+++ SHA1HashGenerator.js
@@ -1,6 +1,14 @@
 const input=document.querySelector('#input');
 const output=document.querySelector('#output');
 const button=document.querySelector('#generate-button');
+const resetButton=document.querySelector('#reset-button');
+
+resetButton.addEventListener('click',()=>{
+  input.value="";
+  output.value="";
+  });
+
+
 button.addEventListener('click',()=>{
   output.value=CryptoJS.SHA1(input.value);
 });
